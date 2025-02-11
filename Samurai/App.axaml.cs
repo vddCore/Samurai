84d41736466bb3cb2e4820a0d5df120e98ee6c14
vddCore <vddcore@outlook.com>
2025-02-11 08:51:35 +0100

    Initial commit.

---
+++ Samurai/App.axaml.cs
@@ -0,0 +1,25 @@
+namespace Samurai;
+
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using PropertyChanged;
+
+[DoNotNotify]
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new MainWindow();
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
